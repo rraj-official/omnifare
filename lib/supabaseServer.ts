7d5940aca77cea491fd5823e627ef705f10a166e
Rohit Raj <rraj.official5@gmail.com>
2026-03-03 13:26:27 +0530

    fixed supabase login issues

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,4 +1,4 @@
-import { createSmartClient as createClient } from "supadns-js";
+import { createClient } from "@supabase/supabase-js";
 import type { SupabaseClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
